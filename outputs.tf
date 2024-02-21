322db060bdf2c314b2590289e2459e586d77ee71
Rupal Sharma <rupal.sharma@clouddrove.com>
2024-02-21 17:50:33 +0530

    fix:change in count condition

--- outputs.tf
+++ outputs.tf
@@ -1,40 +1,40 @@
 output "firewall_id" {
   description = "Firewall generated id"
-  value       = join("", azurerm_firewall.firewall.*.id)
+  value       = try( azurerm_firewall.firewall[*].id, null)
 }
 
 output "firewall_name" {
-  value       = join("", azurerm_firewall.firewall.*.name)
+  value       = try( azurerm_firewall.firewall[*].name, null)
   description = "Firewall name"
 
 }
 
 output "private_ip_address" {
-  value       = azurerm_firewall.firewall[*].ip_configuration[0].private_ip_address
+  value       = try( azurerm_firewall.firewall[*].ip_configuration[0].private_ip_address, null)
   description = "Firewall private IP"
 
 }
 
 output "public_ip_id" {
-  value = azurerm_public_ip.public_ip.*.id
+  value = try(azurerm_public_ip.public_ip[*].id, null)
 }
 
 output "public_ip_address" {
-  value = azurerm_public_ip.public_ip.*.ip_address
+  value = try(azurerm_public_ip.public_ip[*].ip_address, null)
 }
 
 output "firewall_policy_id" {
-  value = join("", azurerm_firewall_policy.policy.*.id)
+  value = try(azurerm_firewall_policy.policy[*].id, null)
 }
 
 output "prefix_public_ip_id" {
-  value = azurerm_public_ip.prefix_public_ip.*.id
+  value = try(azurerm_public_ip.prefix_public_ip[*].id, null)
 }
 
 output "prefix_public_ip_address" {
-  value = azurerm_public_ip.prefix_public_ip.*.ip_address
+  value = try(azurerm_public_ip.prefix_public_ip[*].ip_address, null)
 }
 
 output "public_ip_prefix_id" {
-  value = join("", azurerm_public_ip_prefix.pip-prefix.*.id)
+  value = try(azurerm_public_ip_prefix.pip-prefix[*].id, null)
 }
