194b2bea6708a97c15991aac5e61ac9a73d90c5a
Ravi Malvia <ravi.malvia@clouddrove.com>
2024-07-18 22:51:57 +0530

    fix: subnet version id update to '1.1.0' and Added List in 'specific_subnet_names'

--- _example/firewall-with-isolated-rules/example.tf
+++ _example/firewall-with-isolated-rules/example.tf
@@ -42,7 +42,7 @@ module "vnet" {
 module "name_specific_subnet" {
   depends_on           = [module.vnet]
   source               = "clouddrove/subnet/azure"
-  version              = "1.0.2"
+  version              = "1.1.0"
   name                 = local.name
   environment          = local.environment
   resource_group_name  = module.resource_group.resource_group_name
@@ -50,7 +50,7 @@ module "name_specific_subnet" {
   virtual_network_name = join("", module.vnet.vnet_name)
   #subnet
   specific_name_subnet  = true
-  specific_subnet_names = "AzureFirewallSubnet"
+  specific_subnet_names = ["AzureFirewallSubnet"]
   subnet_prefixes       = ["10.0.1.0/24"]
   # route_table
   routes = [
